567dfdc4b9526fab6bf55a61e30c554a813e3254
BiancaBianchi <biabianchi7398@gmail.com>
2020-09-21 18:08:46 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+#pede um número
+puts("Fale um número")
+#guarda esse numero em uma variavel
+numero = gets()
+#muda o numero para inteiro
+inteiro = numero.to_i()
+#class mostra o tipo de dado 
+puts(numero.class)
