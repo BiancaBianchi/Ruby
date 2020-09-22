3375f17efbd00a2e905f3d59748b17bf830e4cee
BiancaBianchi <69967366+BiancaBianchi@users.noreply.github.com>
2020-09-21 23:44:58 -0300

    Create produto.rb

---
+++ Projeto1/produto.rb
@@ -0,0 +1,8 @@
+#1- No arquivo produto.rb, crie uma classe chamada Produto com os atributos: nome e preço.
+
+class Produto
+  def initialize(nome, preco)
+    @nome = nome
+    @preco = preco
+  end
+end
