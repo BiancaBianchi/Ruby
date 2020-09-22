6150d7b15612c9f2e45954da91f7e2fc1b8ef679
BiancaBianchi <69967366+BiancaBianchi@users.noreply.github.com>
2020-09-21 23:46:47 -0300

    Update produto.rb

--- Projeto1/produto.rb
+++ Projeto1/produto.rb
@@ -1,8 +1,5 @@
 #1- No arquivo produto.rb, crie uma classe chamada Produto com os atributos: nome e preço.
 
 class Produto
-  def initialize(nome, preco)
-    @nome = nome
-    @preco = preco
-  end
+ attr_accessor :nome, :preco
 end
