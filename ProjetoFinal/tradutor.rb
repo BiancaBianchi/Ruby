952e54a334c220b9d29f5efe6db3f93dfb50f75a
BiancaBianchi <69967366+BiancaBianchi@users.noreply.github.com>
2020-09-23 20:41:58 -0300

    Create tradutor.rb
    Projeto final curso Ruby Puro OneBitCode
    Crie um software que permita que o usuário entre com um texto, o idioma em que o texto está escrito e também o idioma para qual ele deseja traduzir o texto e receba como resposta na tela o texto traduzido.
    
     
    
    Critérios:
    
    Você deve utilizar orientação a objetos no seu software
    O resultado de cada tradução (junto com a frase original) deve ser guardado em um arquivo que possua no nome a data e horário da tradução (exp: 10-10-18_10:30.txt)

---
+++ ProjetoFinal/tradutor.rb
@@ -0,0 +1,37 @@
+equire 'rest-client'
+require 'json'
+
+# Classe Tradutor
+class Tradutor
+  attr_reader :texto_traduzido
+  def initialize(texto, idioma)
+    @url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    @key = 'trnsl.1.1.20180922T041958Z.83d0d38d79524d55.a0cf61ce51777dea5cc3cf89a28d4a554588331d'
+    @texto = texto
+    @idioma = 'pt-' + idioma
+    @texto_traduzido = traduzir
+  end
+
+  def escrever
+    time = Time.new
+    nome_arquivo = time.strftime('%d-%m-%y_%H:%M') + '.txt'
+    File.open(nome_arquivo, 'w') do |line|
+      line.puts @texto
+      line.print @texto_traduzido
+    end
+  end
+
+  def traduzir
+    response = RestClient.get(@url, params:
+      { key: @key, text: @texto, lang: @idioma })
+    JSON.parse(response).values.flatten.last
+  end
+end
+
+if ARGV.empty?
+  puts 'Digite o texto e a sigla do idioma para tradução'
+else
+  texto = Tradutor.new(ARGV[0], ARGV[1])
+  puts(texto.texto_traduzido)
+  texto.escrever
+end
